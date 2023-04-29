b3903baed956f03934c8a28a7f0f80ab113da345
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-29 14:29:57 +0300

    Added url controller

---
+++ src/url/url.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express'
+
+import { UrlService } from './url.service'
+
+export class UrlController {
+	constructor(private readonly urlService: UrlService) {}
+
+	async requestOnUrl(req: Request, res: Response) {
+		try {
+			const { slug } = req.params
+			const origin = await this.urlService.requestOnUrl(slug)
+			res.status(302).redirect(origin)
+		} catch (error) {
+			if (error instanceof ServerException) {
+				res.status(error.status).json({message: error.message})
+			} else {
+				res.status(500).json({message: 'unexpected server error'})
+			}
+		}
+	}
+}
