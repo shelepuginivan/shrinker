bc37c151fd1bcfa23762c13f239b676b608e3fc4
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-29 16:37:41 +0300

    Log only unexpected errors

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -3,11 +3,10 @@ import { ErrorRequestHandler, NextFunction, Request, Response } from 'express'
 import { ServerException } from '../exceptions/server.exception'
 
 export const errorMiddleware: ErrorRequestHandler = (error: unknown, _req: Request, res: Response, _next: NextFunction) => {
-	console.error(error)
-
 	if (error instanceof ServerException) {
 		res.status(error.status).json({ message: error.message })
 	} else {
+		console.error(error)
 		res.status(500).json({ message: 'unexpected server error' })
 	}
 }
