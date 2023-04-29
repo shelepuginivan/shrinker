d5af63529e6a45fe233680ad9851dd437daa6e21
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-29 16:00:45 +0300

    Added field `isDefault` to not duplicate uuids with same origin

--- src/url/url.model.ts
+++ src/url/url.model.ts
@@ -2,7 +2,8 @@ import { model, Schema } from 'mongoose'
 
 const urlSchema = new Schema({
 	origin: { type: String, required: true },
-	slug: { type: String, required: true }
+	slug: { type: String, required: true },
+	isDefault: { type: Boolean, default: false }
 })
 
 export default model('Url', urlSchema)
