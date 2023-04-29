b8caf850ed6882a0280f00ec7ed508c22651307b
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-29 16:38:08 +0300

    Added shortenedLink field

--- src/url/url.ts
+++ src/url/url.ts
@@ -1,9 +1,15 @@
+import config from 'config'
+
 export class Url {
 	origin: string
 	slug: string
+	shortenedLink: string
 
 	constructor(origin: string, slug: string) {
+		const host = config.get('host') || ''
+
 		this.origin = origin
 		this.slug = slug
+		this.shortenedLink = `${host}/${slug}`
 	}
 }
