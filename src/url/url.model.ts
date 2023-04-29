3bed9eadf17f22ac95214094c2bd0ec302b18e4e
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-29 16:38:56 +0300

    Added `unique: true` in slug field

--- src/url/url.model.ts
+++ src/url/url.model.ts
@@ -2,7 +2,7 @@ import { model, Schema } from 'mongoose'
 
 const urlSchema = new Schema({
 	origin: { type: String, required: true },
-	slug: { type: String, required: true },
+	slug: { type: String, required: true, unique: true },
 	isDefault: { type: Boolean, default: false }
 })
 
