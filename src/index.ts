349934504c145bc487a1641c0f0960e0a618311f
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-29 15:35:02 +0300

    Added middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
+import bodyParser from 'body-parser'
 import config from 'config'
-import express from 'express'
+import express, { json } from 'express'
 import { connect } from 'mongoose'
 
+import { errorMiddleware } from './middlewares/error.middleware'
+import urlRouter from './url/url.router'
 
 const app = express()
 
+app.use(bodyParser.json())
+app.use(json())
+app.use('/', urlRouter)
+app.use(errorMiddleware)
 
 const start = async () => {
 	const port = config.get('port') as number ?? 8000
